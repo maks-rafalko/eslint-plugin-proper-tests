9ff6788492289a0b7ff9b973d58c9808e7306a21
Maks Rafalko <maks.rafalko@gmail.com>
2024-07-02 00:39:44 +0200

    Add new rule `no-mixed-expectation-groups`
    This rule disallows mixing expectations for different variables between each other.
    
    The following patterns are considered errors:
    
    ```ts
    // example 1
    expect(foo).toBeDefined();
    expect(bar).toBeNull();
    expect(foo).toHaveLength(3); // should be placed before "bar"
    
    // example 1
    expect(foo).toBeDefined();
    expect(bar).toBeNull();
    expect(foo.bars).toHaveLength(3); // should be placed before "bar"
    ```
    
    The following patterns are considered correct:
    
    ```ts
    // example 1
    expect(foo).toBeDefined();
    expect(foo).toHaveLength(3);
    
    expect(bar).toBeNull();
    
    // example 2
    expect(foo).toBeDefined();
    expect(foo.bars).toHaveLength(3);
    
    expect(bar).toBeNull();
    ```

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,7 @@
 import type { Linter } from '@typescript-eslint/utils/ts-eslint';
 
 import recommended from './configs/recommended';
+import { noMixedExpectationGroups } from './custom-rules/no-mixed-expectation-groups';
 import { noUselessMatcherToBeDefined } from './custom-rules/no-useless-matcher-to-be-defined';
 import { noUselessMatcherToBeNull } from './custom-rules/no-useless-matcher-to-be-null';
 
@@ -11,5 +12,6 @@ export = {
   rules: {
     'no-useless-matcher-to-be-defined': noUselessMatcherToBeDefined,
     'no-useless-matcher-to-be-null': noUselessMatcherToBeNull,
+    'no-mixed-expectation-groups': noMixedExpectationGroups,
   },
 } satisfies Linter.Plugin;
